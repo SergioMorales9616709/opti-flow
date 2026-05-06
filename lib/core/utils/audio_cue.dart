9a692ef53039a4faf26a33a7a1f6b48b1b8e2923
Sergio Morales <envidiom@gmail.com>
2026-05-05 21:35:16 -0600

    feat(audio): add success AudioCue

--- lib/core/utils/audio_cue.dart
+++ lib/core/utils/audio_cue.dart
@@ -1,6 +1,7 @@
 enum AudioCue {
   click('audio/click.mp3', isBgm: false),
-  bgmFlow('audio/bgm_flow.mp3', isBgm: true);
+  bgmFlow('audio/bgm_flow.mp3', isBgm: true),
+  success('audio/success.mp3', isBgm: false);
 
   const AudioCue(this.path, {required this.isBgm});
 
